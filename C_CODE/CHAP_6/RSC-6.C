29d305d20abca2e872e1d22257afc390503a0e23
AbdullahTrees <43266058+AbdullahTrees@users.noreply.github.com>
2020-09-05 02:34:17 +0600

    RSC-6 recovery!!!

---
+++ C_CODE/CHAP_6/RSC-6.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char phonebook[][2][80] = {
+											"Shikamaru", 				"01189232875",
+											"Pascal Bhai", 			"20948103194",
+											"Mamu", 						"029293498",
+											"Sivaji b0ss", 			"00123011551",
+											"Ahmed", 					"91235381273",
+											"Meme dealer Abrar", 	"029183572",
+											"Tilapia", 					"028593456",
+											"Raizan", 					"024348556",
+											"Captain Black", 			"28103892325",
+											"Ammu", 						"18180348769",
+											"Nana bhai", 				"029384234"
+										}
+
+	int i;
+
+	printf("Da Telephone book of kewl people\n");
+	printf("Enter a name
+
+
+
+}
