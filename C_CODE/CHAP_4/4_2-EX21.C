876589a7137c7494fc18d0f5aee5303696684c44
AhnafAbdullah_IUB <124304231+cinco-de-mayonnaise@users.noreply.github.com>
2023-10-12 19:05:45 +0600

    Update 4_2-EX21.C

--- C_CODE/CHAP_4/4_2-EX21.C
+++ C_CODE/CHAP_4/4_2-EX21.C
@@ -2,19 +2,28 @@
 
 float soundspeed(unsigned long d);
 
+
 int main(void)
 {
-	unsigned long distance;
 
-	printf("\nGib distance(feet) and I'll tell you how long it takes for sound to travel that distance: ");
-	scanf("%lu", &distance);
+	unsigned long d;
 
-	printf("\nIt takes sound %g seconds to travel that distance. ", soundspeed(distance));
+
+	printf("\nGive distance(foot) and I'll tell you how long it takes for sound to travel that distance: ");
+	scanf("%lu", &d);
+
+
+	printf("\nIt takes sound %g seconds to travel that distance. ", soundspeed(d));
+
 
 	return 0;
+
+
 }
 
 float soundspeed(unsigned long d)
 {
+
 	return d / 1129.0;
+
 }
